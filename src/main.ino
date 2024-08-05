035898e18583d47f644e5edc4e846d161cc1fc81
AlexPhongsavath <anutsaya@icloud.com>
2024-08-05 15:42:59 +0700

    Update

--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,7 @@
 /*
 
-Source code is locate into Example  Folder
-If you want to run project, Copy code and run into main.ino each node
+Source code is locate into "Heltect_LoRaP2P_For_WaterLevel\Example\Source-Code"
+If you want to run project, Copy code (Node-A.ino & Node-B.ino) and run into main.ino each node
 
 */
 
